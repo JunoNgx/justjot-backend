01f4fdb311959c007e8dbe95ae5c91fec5cf12da
Juno Nguyen <juno.ngx@gmail.com>
2024-03-09 23:30:04 +0800

    Implement sending verification email after registration

---
+++ pb_hooks/main.pb.js
@@ -0,0 +1,8 @@
+/// <reference path="../pb_data/types.d.ts" />
+
+// Send email verification after a new user record is created
+onRecordAfterCreateRequest((e) => {
+    const userId = e.record.id;
+    const userRecord = $app.dao.findRecordById("users", userId);
+    $mails.sendRecordVerification(userId, userRecord);
+}, "users");
