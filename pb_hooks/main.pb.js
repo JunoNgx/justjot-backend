a3f89cd2bda1a263245fa2b26e750add0dd15df1
Juno Nguyen <juno.ngx@gmail.com>
2024-03-10 00:10:16 +0800

    Draft create first collection

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -3,8 +3,27 @@
 // Send email verification after a new user record is created
 onRecordAfterCreateRequest((e) => {
     const userId = e.record.id;
-    const userRecord = $app.dao.findRecordById("users", userId);
-    $mails.sendRecordVerification(userId, userRecord);
+    // const userRecord = $app.dao.findRecordById("users", userId);
+    // $mails.sendRecordVerification(userId, userRecord);
+
+    // Create First Collection
+    const itemCollectionsCollection = $app.dao().findCollectionByNameOrId("itemCollections");
+    const newItemCollectionRecord = new Record(itemCollectionsCollection);
+    const form = new RecordUpsertForm($app, newItemCollectionRecord);
+
+    form.loadData({
+        owner: userId,
+        name: "First Collection",
+        slug: "first-collection",
+        sortOrder: 0,
+    });
+
+    form.submit();
 }, "users");
 
-// TODO: fetch title and favicon for links
+// // Create starting data after a new user record is verified
+// onRecordAfterConfirmVerificationRequest((e) => {
+    
+// }, "users");
+    
+// TODO: fetch title and favicon for linksa
