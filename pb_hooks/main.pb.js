a4c052551c54dd57265d0564ad43483dd147fa66
Juno Nguyen <juno.ngx@gmail.com>
2024-03-10 00:43:58 +0800

    Implement creating other items

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -39,38 +39,38 @@ onRecordAfterCreateRequest((e) => {
     });
     linkItemForm.submit();
 
-    // // Create colour
-    // const colourItemRecord = new Record(itemCollection);
-    // const colourItemForm = new RecordUpsertForm($app, colourItemRecord);
-    // colourItemForm.loadData({
-    //     owner: userId,
-    //     collection: firstCollectionId,
-    //     title: "Medium Aquamarine"
-    //     content: "#66CDAA",
-    // });
-    // colourItemForm.submit();
+    // Create colour
+    const colourItemRecord = new Record(itemCollection);
+    const colourItemForm = new RecordUpsertForm($app, colourItemRecord);
+    colourItemForm.loadData({
+        owner: userId,
+        collection: firstCollectionId,
+        title: "Medium Aquamarine",
+        content: "#66CDAA",
+    });
+    colourItemForm.submit();
 
-    // // Create short note
-    // const shortNoteItemRecord = new Record(itemCollection);
-    // const shortNoteItemForm = new RecordUpsertForm($app, shortNoteItemRecord);
-    // shortNoteItemForm.loadData({
-    //     owner: userId,
-    //     collection: firstCollectionId,
-    //     title: "Short note"
-    //     content: "Click here to copy",
-    // });
-    // shortNoteItemForm.submit();
+    // Create short note
+    const shortNoteItemRecord = new Record(itemCollection);
+    const shortNoteItemForm = new RecordUpsertForm($app, shortNoteItemRecord);
+    shortNoteItemForm.loadData({
+        owner: userId,
+        collection: firstCollectionId,
+        title: "Short note",
+        content: "Click here to copy",
+    });
+    shortNoteItemForm.submit();
 
-    // // Create long note
-    // const longNoteItemRecord = new Record(itemCollection);
-    // const longNoteItemForm = new RecordUpsertForm($app, longNoteItemRecord);
-    // longNoteItemForm.loadData({
-    //     owner: userId,
-    //     collection: firstCollectionId,
-    //     title: "Long note"
-    //     content: "Click here to open the editor. Notes with more than 50 characters will have their default action automatically set to open the editor. You can manually change this behavior from the context menu (right click on a mouse; long press from a touchscreen, or Cmd/Ctrl + M from a keyboard).
-    // });
-    // longNoteItemForm.submit();
+    // Create long note
+    const longNoteItemRecord = new Record(itemCollection);
+    const longNoteItemForm = new RecordUpsertForm($app, longNoteItemRecord);
+    longNoteItemForm.loadData({
+        owner: userId,
+        collection: firstCollectionId,
+        title: "Long note",
+        content: "Click here to open the editor. Notes with more than 50 characters will have their default action automatically set to open the editor. You can manually change this behavior from the context menu (right click on a mouse; long press from a touchscreen, or Cmd/Ctrl + M from a keyboard)."
+    });
+    longNoteItemForm.submit();
 
 }, "users");
 
