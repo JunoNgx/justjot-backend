97384f9004783c9a0664ea6eaa411995f632c82d
Juno Nguyen <juno.ngx@gmail.com>
2024-03-13 00:14:20 +0800

    Implement refetch route

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"justjot-backend/funcs"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 
+	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -15,6 +19,29 @@ func main() {
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.POST("/refetch/:itemId", func(c echo.Context) error {
+			itemId := c.PathParam("itemId")
+			fmt.Println("refetch", itemId)
+			itemRecord, err := app.Dao().FindRecordById("items", itemId)
+			if err != nil {
+				return c.JSON(http.StatusNotFound, map[string]string{"message": "requested item not found"})
+			}
+
+			processedUrl := funcs.TryAddProtocolToUrl(itemRecord.Get("content").(string))
+			_, err = url.ParseRequestURI(processedUrl)
+			if err != nil {
+				return c.JSON(http.StatusNoContent, map[string]string{"message": "content is not valid url"})
+			}
+
+			funcs.TryFetchTitleAndFavicon(app, itemRecord, processedUrl)
+
+			return c.JSON(http.StatusOK, map[string]string{"message": "Refetch successful"})
+		})
+
 		return nil
 	})
 
