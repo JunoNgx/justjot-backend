d38b40f05c9c2a005ab710912419b89b57f897d7
Juno Nguyen <juno.ngx@gmail.com>
2024-03-17 00:03:19 +0800

    Add refetch route

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -2,6 +2,35 @@
 // ES5 dynamic import to accomodate Goja caveats
 const types = require(`${__hooks}/types.js`);
 
+routerAdd("POST", "/refetch/:itemId", c => {
+    const utils = require(`${__hooks}/utils.js`);
+    const funcs = require(`${__hooks}/funcs.js`);
+
+    const itemId = c.pathParam("itemId");
+
+    try {
+        const itemRecord = $app.dao().findRecordById(itemId);
+        const isValidUrl = utils.isValidUrl(itemRecord.content);
+        if (isValidUrl) {
+            funcs.tryGetTitleAndFavicon(itemRecord);
+            return;
+        }
+
+        $app.logger().warn(
+            "Was requested to refetch, but not a valid url",
+            "itemId", itemId,
+            "content", content,
+        );
+
+    } catch (err) {
+        $app.logger().error(
+            "Error processing request to refetch hyperlink",
+            "itemId", itemId,
+            "error", err,
+        );
+    }
+});
+
 onRecordAfterCreateRequest((e) => {
     const funcs = require(`${__hooks}/funcs.js`);
 
