f0ba7e54d85e938a4974281e11e027f30e5dcfef
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 13:06:47 +0800

    Draft utils.processNewItems()

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -14,71 +14,7 @@ onRecordAfterCreateRequest((e) => {
 // }, "users");
 
 onModelAfterCreate(async (e) => {
-    const SHORT_NOTE_LEN = 50;
-    const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-        '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-
-    const itemRecordId = e.model.id;
-    const itemRecord = $app.dao().findFirstRecordByData("items", "id", itemRecordId);
-    const content = itemRecord.get("content");
-    const form = RecordUpsertForm($app, itemRecord);
-
-    // // Is todo list
-    // const firstChar = content.substring(0, 1);
-    // if (firstChar === "*" || firstChar === "-") {
-    //     // Use submitted content as title
-    //     form.loadData({
-    //         type: "todoList",
-    //         title: content,
-    //         content: ""
-    //     });
-    //     form.submit();
-    //     return;
-    // }
-
-    // Is link
-    const isValidUrl = urlPattern.test(content);
-    if (isValidUrl) {
-        // fetch doesn't work in Goja :<
-        // const getTitle = async (url) => {  
-        //     return fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
-        //         .then(response => {
-        //             $app.logger().info(response);
-        //             if (response.ok) return response.json()
-        //             $app.logger().error("Failed to fetch data from url", response);
-        //         })
-        //         .then(data => {
-        //             const doc = new DOMParser().parseFromString(data.contents, "text/html");
-        //             const title = doc.querySelectorAll('title')[0];
-        //             return title.innerText;
-        //         });
-        // };
-        
-        let title;
-        // try {
-        //     title = await getTitle(content);
-        // } catch (error) {
-        //     $app.logger().error("error fetching title", error);
-        // }
-        // TODO: fetch title and favicon
-        form.loadData({
-            type: "link",
-            title
-        });
-        form.submit();
-        return;
-    }
-
-    // Is note
-    const formData = {
-        type: "text"
-    };
-    if (content.length <= SHORT_NOTE_LEN) formData.shouldCopyUponClick = true;
-    form.loadData(formData);
-    form.submit();
-
-}, "items")
+    const utils = require(`${__hooks}/utils.js`);
+    
+    utils.processNewItem(e);
+}, types.DbTables.ITEMS);
