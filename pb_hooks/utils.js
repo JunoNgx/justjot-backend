920ed3e0899ceba2b6fcb5bbd4d695294756642e
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 12:15:21 +0800

    Implement DbTables enum

--- pb_hooks/utils.js
+++ pb_hooks/utils.js
@@ -1,6 +1,12 @@
+const DbTables = {
+    USERS: "users",
+    ITEMS: "items",
+    COLLECTIONS: "itemCollections"
+};
+
 const utils = {
     sendUserEmailVerification(userId) {
-        const userRecord = $app.dao().findRecordById("users", userId);
+        const userRecord = $app.dao().findRecordById(DbTables.USERS, userId);
         $mails.sendRecordVerification($app, userRecord);
     }
 
