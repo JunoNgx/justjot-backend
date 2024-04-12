e0a9ed457879ead55436eabec597725d90aec894
Juno Nguyen <juno.ngx@gmail.com>
2024-04-12 23:39:34 +0800

    Flesh out logic for demoFuncs

--- pb_hooks/demoFuncs.js
+++ pb_hooks/demoFuncs.js
@@ -1,14 +1,47 @@
 /// <reference path="../pb_data/types.d.ts" />
 
-const he = require(`${__hooks}/libs/he.js`);
-
 const types = require(`${__hooks}/types.js`);
 const consts = require(`${__hooks}/consts.js`);
 const utils = require(`${__hooks}/utils.js`);
 
 const demoFuncs = {
     resetDemoData() {
-        console.log("reset demo data");
+        const demoUser = $app.dao().findAuthRecordByUsername(
+            types.DbTables.USERS,
+            consts.DEMO_USERNAME
+        );
+
+        demoFuncs.resetDisplayName(demoUser);
+
+        const demoUserId = demoUser.getId();
+        demoFuncs.deleteCollections(demoUserId);
+    },
+
+    resetDisplayName(demoUser) {
+        const form = new RecordUpsertForm($app, demoUser);
+        form.loadData({
+            displayName: "Jay Doe"
+        });
+        form.submit();
+    },
+
+    deleteCollections(demoUserId) {
+        const itemCollections = $app.dao().findRecordsByFilter(
+            types.DbTables.COLLECTIONS,
+            `owner=${demoUserId}`,
+        );
+
+        for (const coll of itemCollections) {
+            $app.dao().deleteRecord(coll);
+        };
+    },
+
+    createCollections() {
+
+    },
+
+    createItems() {
+
     },
 };
 
