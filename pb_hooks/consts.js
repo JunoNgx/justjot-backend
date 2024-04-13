be95be3f752316463332bd655778c8140792c672
Juno Nguyen <juno.ngx@gmail.com>
2024-04-13 21:39:40 +0800

    Add new constant variables

--- pb_hooks/consts.js
+++ pb_hooks/consts.js
@@ -4,6 +4,9 @@ const consts = {
     ERROR_NEW_USER: "[ERROR: handle new user] ",
     ERROR_NEW_ITEM: "[ERROR: handle new item] ",
     TEST_ACC_USERNAME: "JayDoeTest",
+    TEST_ACC_DISPLAY_NAME: "Jay Doe",
+    TEST_ACC_EMAIL: "asdfijkl@email.com",
+    TEST_ACC_PASSWORD: "password123",
 }
 
 module.exports = consts;
