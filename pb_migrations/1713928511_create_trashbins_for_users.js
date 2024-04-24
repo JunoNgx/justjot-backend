b12bfc6ae824fdfe817a8f022d9303687d194bc0
Juno Nguyen <juno.ngx@gmail.com>
2024-04-24 14:34:05 +0800

    Clean up migration logic for creating trash bins

--- pb_migrations/1713928511_create_trashbins_for_users.js
+++ pb_migrations/1713928511_create_trashbins_for_users.js
@@ -1,30 +1,11 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 migrate((db) => {
-    console.log("migrate up")
     const dao = new Dao(db);
 
-    // const getAllRecords = (collectionName) => {
-    //     const records = arrayOf(new Record);
-
-    //     // dao.recordQuery(collectionName)
-    //     //     .all(records);
-
-    //     $app.dao().recordQuery(collectionName)
-    //         // .andWhere($dbx.hashExp({ "status": "active" }))
-    //         // .orderBy("published DESC")
-    //         .limit(10)
-    //         .all(records)
-
-    //     return records;
-    // };
-
-    // const usersCollection = dao.findCollectionByNameOrId("users");
     const trashBinsCollection = dao.findCollectionByNameOrId("trashBins");
-
     // TODO: rename to `findAllRecords` in the next release
     const allUsers = dao.findRecordsByExpr("users");
-    // const allUsers = getAllRecords("users");
 
     for (const user of allUsers) {
 
