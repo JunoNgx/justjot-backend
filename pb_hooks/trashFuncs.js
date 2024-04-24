a64f3e1d27c54578ce82fc7663c0a0039982ebdf
Juno Nguyen <juno.ngx@gmail.com>
2024-04-24 21:42:27 +0800

    Implement handlejexpiredjtrashTimes

--- pb_hooks/trashFuncs.js
+++ pb_hooks/trashFuncs.js
@@ -59,6 +59,38 @@ const trashFuncs = {
             return c.json(500, { error: err });
         }
     },
+
+    handleExpiredTrashItems() {
+        $app.logger().info(
+            "Performing daily check of trashed items",
+            "datetime", new Date().toISOString()
+        );
+
+        const allItems = $app.dao().findRecordsByExpr(types.DbTables.ITEMS);
+
+        for (const item of allItems) {
+            if (!item.getBool("isTrashed")) {
+                continue;
+            }
+
+            try {
+                const sevenDaysAgoTime = new DateTime().time().addDate(0, 0, -7);
+                const trashedTime = item.getDateTime("trashedDateTime").time();
+                if (trashedTime.compare(sevenDaysAgoTime) === 0
+                    || trashedTime.compare(sevenDaysAgoTime) === -1
+                ) {
+                    $app.dao().deleteRecord(item);
+                }
+            } catch (err) {
+                $app.logger().error(
+                    "Error attempting to delete trashed item",
+                    "itemId", item.id,
+                    "error", err.toString(),
+                );
+                console.log(err.toString());
+            }
+        }
+    },
 };
 
 module.exports = trashFuncs;
